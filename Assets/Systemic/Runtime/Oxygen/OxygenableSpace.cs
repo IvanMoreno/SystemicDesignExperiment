6d0e8b5b1d1c8b2ebd05d0255726cc833e569411
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-05-10 17:49:22 +0200

    feat(oxygen): remove oxygen in oxygenable space by breathing

---
+++ Assets/Systemic/Runtime/Oxygen/OxygenableSpace.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sistemic.Runtime.Oxygen
+{
+    public class OxygenableSpace : MonoBehaviour
+    {
+        [SerializeField] float spaceRadius = 1;
+        [SerializeField] float oxygenAccumulationDecrementMultiplier = 1;
+        [SerializeField] float accumulatedOxygen = 1;
+        
+        public float AccumulatedOxygen
+        {
+            get => accumulatedOxygen;
+            private set => accumulatedOxygen = Mathf.Clamp(value, 0, 1);
+        }
+
+        void Update()
+        {
+            if (accumulatedOxygen > 0) 
+                EmitToElementsInsideArea();
+
+            GetComponentInChildren<SpriteRenderer>().color = Color.Lerp(Color.white, Color.blue, 1 - AccumulatedOxygen);
+        }
+
+        void EmitToElementsInsideArea()
+        {
+            foreach (var potentialReceiver in NearbyElements())
+            {
+                EmitTo(potentialReceiver.gameObject);
+            }
+        }
+
+        IEnumerable<Collider2D> NearbyElements() => Physics2D.OverlapCircleAll(transform.position, spaceRadius);
+
+        void EmitTo(GameObject potentialReceiver)
+        {
+            potentialReceiver.SendMessage
+            (
+                "Perceive",
+                new Dictionary<string, object>
+                {
+                    { "signal", "Oxygen" }
+                },
+                SendMessageOptions.DontRequireReceiver
+            );
+        }
+
+        void Perceive(Dictionary<string, object> stimuli)
+        {
+            if (stimuli["signal"].Equals("CarbonDioxide"))
+            {
+                DecreaseOxygen();
+            }
+        }
+
+        void DecreaseOxygen() => AccumulatedOxygen -= Time.deltaTime * oxygenAccumulationDecrementMultiplier;
+        void OnDrawGizmos() => Gizmos.DrawWireSphere(transform.position, spaceRadius);
+    }
+}
